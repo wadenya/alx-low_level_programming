d4925730b89dd7123d8cf28ad6101d123f7436e9
wadenya <alexwadenya001@gmail.com>
2022-12-09 20:41:12 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,11 +13,13 @@ int main(void)
 {
 	int n;
 
-	for (n - 48; n < 58; n++)
+	for (n = 0; n < 9; n++)
 	{
-		putchar(n);
-		if (n != 57)
-		{
+		putchar(n % 10) + '0');
+		if (n == 9)
+
+			continue;
+
 			putchar(',');
 			putchar(' ');
 		}
