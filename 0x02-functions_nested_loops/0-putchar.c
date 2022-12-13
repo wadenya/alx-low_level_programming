b05e1264ac5edaa92e1ea71ee632ae30095e4407
wadenya <alexwadenya001@gmail.com>
2022-12-13 02:56:53 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+
+/**
+ * main - print _putchar and a new line
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+
+{
+	-putchar('_');
+	-putchar('p');
+	-putchar('u');
+	-putchar('t');
+	-putchar('c');
+	-putchar('h');
+	-putchar('a');
+	-putchar('r');
+	-putchar('\n');
+
+	return (0);
+}
