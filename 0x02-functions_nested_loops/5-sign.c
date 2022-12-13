20edb52103151768971fb0f7be1c21c4153d91c4
wadenya <alexwadenya001@gmail.com>
2022-12-13 08:08:32 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+
+/**
+ * print_sign - prints sign of a number
+ *
+ * @n: the number of sign to be printed
+ *
+ * Return: 1 if number is greater than zero
+ *
+ * 0 for number zero
+ *
+ * -1 if number is less than 0
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > o)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('_');
+		return (-1);
+	}
+}
