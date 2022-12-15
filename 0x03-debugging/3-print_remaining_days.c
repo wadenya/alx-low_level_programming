2b2666a5eece50b776a3597ecbb848817d6f8501
wadenya <alexwadenya001@gmail.com>
2022-12-15 11:38:01 -0800

    Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 	{
 	if (month > 2 && day >= 60)
 	{
@@ -32,8 +32,8 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 365 - day);
 	}
-       }
-}	
-		
+	}
+}
+
 
 
