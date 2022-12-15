7a5fd923dc0d6373e25666516d147d885be1b1f0
wadenya <alexwadenya001@gmail.com>
2022-12-15 06:31:54 -0800

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,11 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	l = 0;
-	/**
-	 *while (i < 10)
-	 *{
-	 *	putchar(i);
-	 *}
-	 */
+
+	 /*while (i < 10)*/
+	 /*{*/
+	 /* putchar(i);*/
+	 /*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
