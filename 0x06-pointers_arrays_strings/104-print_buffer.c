490f0d84a36c795e0688b41f38afda063c314d59
wadenya <alexwadenya001@gmail.com>
2022-12-22 15:06:16 -0800

    Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,41 +10,41 @@
  */
 void print_buffer(char *b, int size)
 {
-int h, index;
+	int h, index;
 
-for (h = 0; h < size; h += 10)
-{
-printf("%08x:", h);
+	for (h = 0; h < size; h += 10)
+	{
+		printf("%08x:", h);
 
-for (index = 0; index < 10; index++)
-{
-if ((index + h) >= size)
-printf(" ");
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + h) >= size)
+				printf(" ");
 
-else
-printf("%02x", *(b + index + h));
+			else
+				printf("%02x", *(b + index + h));
 
-if ((index % 2) != 0 && index != 0)
-printf(" ");
-}
+			if ((index % 2) != 0 && index != 0)
+				printf(" ");
+		}
 
-for (index = 0; index < 10; index++)
-{
-if ((index + h) >= size)
-break;
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + h) >= size)
+				break;
 
-else if (*(b + index + h) >= 31 &&
-*(b + index + h) <= 126)
-printf("%c", *(b + index + h));
+			else if (*(b + index + h) >= 31 &&
+				*(b + index + h) <= 126)
+				printf("%c", *(b + index + h));
 
-else
-printf(".");
+			else
+				printf(".");
+		}
+		if (h >= size)
+			continue;
+
+		printf("\n");
 	}
-	if (h >= size)
-		continue;
-
-	printf("\n");
-	}
-if (size <= 0)
-printf("\n");
+	if (size <= 0)
+		printf("\n");
 }
