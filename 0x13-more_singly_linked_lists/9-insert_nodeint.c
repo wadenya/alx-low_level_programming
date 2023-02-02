65e5f9257124df40a8285075f8f33b1095729266
wadenya <alexwadenya001@gmail.com>
2023-02-02 07:48:38 -0800

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,19 +27,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		if (temp == NULL)
 			return (NULL);
-		{
-			new = malloc(sizeof(listint_t));
-			if (new == NULL)
-				return (NULL);
-			new->n = n;
-			if (idx == 0)
-			{
-				new->next = *head;
-				*head = new;
-				return (new);
-			}
-		}
 	}
+		new = malloc(sizeof(listint_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		if (idx == 0)
+		{
+			new->next = *head;
+			*head = new;
+			return (new);
+		}
 	new->next = temp->next;
 	temp->next = new;
 	return (new);
