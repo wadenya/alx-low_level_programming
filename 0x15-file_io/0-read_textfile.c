f6b2e5e58565a74922b2a0c8bbf64579e1c4455e
wadenya <alexwadenya001@gmail.com>
2023-02-08 02:03:33 -0800

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,7 @@
 /**
  * read_textfile - reads a text file and prints it to the POSIX output
  * @filename: pntr to file name
- * letters: number of letters it should read and print
+ * @letters: number of letters it should read and print
  * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
