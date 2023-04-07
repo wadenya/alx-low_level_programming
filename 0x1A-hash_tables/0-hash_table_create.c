1b63d8eb403a24edbfcea3035df7d92fa07c88e9
wadenya <alexwadenya001@gmail.com>
2023-04-07 02:17:34 -0700

    1. djb2

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,8 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table.
+ * @size: size of the array
+ * Return: NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
