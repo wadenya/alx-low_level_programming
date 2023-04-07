b6b4126771d75541f83c8e7eb07a2a9277158282
wadenya <alexwadenya001@gmail.com>
2023-04-07 02:23:56 -0700

    1. djb2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,3 +6,18 @@
  * Return: NULL
  */
 hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+	new_table->size = size;
+	new_table->array = malloc(size * sizeof(hash_node_t *));
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	for (int i = 0; i < size; i++)
+		new_table->array[i] = NULL;
+	return (new_table);
+}
