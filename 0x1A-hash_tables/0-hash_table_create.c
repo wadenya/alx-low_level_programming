1807502c20dd4cfe8abf5776d2d503b5e4d270b7
wadenya <alexwadenya001@gmail.com>
2023-04-07 02:56:11 -0700

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,9 +7,13 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int y;
+
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+
 	if (new_table == NULL)
 		return (NULL);
+
 	new_table->size = size;
 	new_table->array = malloc(size * sizeof(hash_node_t *));
 	if (new_table->array == NULL)
@@ -17,7 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(new_table);
 		return (NULL);
 	}
-	for (int i = 0; i < size; i++)
-		new_table->array[i] = NULL;
+	for (y = 0; y < size; y++)
+		new_table->array[y] = NULL;
 	return (new_table);
 }
