938447a557ec8431887a8d774bf6d3653a46c05b
wadenya <alexwadenya001@gmail.com>
2023-04-07 06:28:12 -0700

    >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: is hash table
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int y;
+	hash_node_t *printer;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (y = 0; y < ht->size; y++)
+	{
+		printer = ht->array[y];
+		while (printer)
+		{
+			printf("'%s': '%s'", printer->key, printer->value);
+			printer = printer->next;
+			if (printer)
+				printf(", ");
+		}
+	}
+	printf("}\n");
+}
