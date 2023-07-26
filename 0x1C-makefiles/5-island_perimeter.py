40888475e3f3b7e2169882fa277f76837d03c7a6
wadenya <alexwadenya001@gmail.com>
2023-07-26 03:33:26 -0700

    0x1C. C - Makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Island Perimeter"""
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described
+        0 represents a water zone
+        1 represents a land zone
+        One cell is a square with side length 1
+        Grid cells are connected horizontally/vertically (not diagonally).
+        Grid is rectangular, width and height don’t exceed 100
+    """
+
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for h in range(height):
+        for w in range(width):
+            if grid[h][w] == 1:
+                size += 1
+                if (w > 0 and grid[h][w - 1] == 1):
+                    edges += 1
+                if (h > 0 and grid[h - 1][w] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
