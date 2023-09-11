4ee3dbd55f40acc6525863ea8cb2ae6f5bda35fc
wadenya <alexwadenya001@gmail.com>
2023-09-11 16:41:26 -0700

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,19 +13,14 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	if (array == NULL)
-	{
+	if (!array || size == 0)
 		return (-1);
-	}
 
 	for (size_t i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-
 		if (array[i] == value)
-		{
 			return (i);
-		}
 	}
 	return (-1);
 }
